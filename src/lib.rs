513534ac0e758ef77876e2e82ea648fba9eb24dd
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2024-01-23 23:19:41 +0900

    `CASE_INSENSITIVE`

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        let case_sensitive = env::var("CASE_SENSITIVE").is_err();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
     }
